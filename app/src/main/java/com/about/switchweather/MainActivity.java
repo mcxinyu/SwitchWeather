87cddd191c24ac68ce2b4322a6a70fbea550da34
Vicent <mcxinyu@foxmail.com>
2016-08-26 17:56:28 +0800

    第二阶段 - 2
    TODO
    4、运行程序获取定位，默认显示当前城市数据；（0%）
    5、搜索目标城市后添加数据；（0%）
    6、获取城市列表存储到数据库；（100%）
    7、国际化；（100%）
    8、修改无网络、无接口数据等处理（100%）
    9、添加透明的 Toolbar；（100%）

--- app/src/main/java/com/about/switchweather/MainActivity.java
+++ app/src/main/java/com/about/switchweather/MainActivity.java
@@ -4,6 +4,12 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBar;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 import com.about.switchweather.Model.City;
 import com.about.switchweather.Model.Condition;
 import com.about.switchweather.Util.HeWeatherFetch;
@@ -12,6 +18,8 @@ import com.about.switchweather.Util.WeatherLab;
 import java.util.List;
 
 public class MainActivity extends SingleFragmentActivity implements MainEmptyFragment.Callbacks {
+    private Toolbar mToolbar;
+    private ActionBar mActionBar;
 
     @Override
     public Fragment createFragment() {
@@ -21,9 +29,37 @@ public class MainActivity extends SingleFragmentActivity implements MainEmptyFra
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
+
+        mActionBar = getSupportActionBar();
+        mToolbar.setNavigationIcon(R.drawable.ic_select_icon);
+
+        mToolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "setNavigationOnClickListener", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        mToolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                Toast.makeText(MainActivity.this, "setOnMenuItemClickListener", Toast.LENGTH_SHORT).show();
+                return false;
+            }
+        });
+
         if (WeatherLab.get(this).getCityList().size() == 0 || WeatherLab.get(this).getConditionBeanList().size() == 0){
             new FetchCondition().execute();
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_list, menu);
+        return true;
     }
 
     @Override
