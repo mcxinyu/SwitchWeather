f0e46ec6cc96a15de6b9bd126b2ad8b64022f30c
Vicent <mcxinyu@foxmail.com>
2016-09-07 13:02:45 +0800

    增加 LogUtils

---
+++ app/src/main/java/com/about/switchweather/Util/LogUtils.java
@@ -0,0 +1,36 @@
+package com.about.switchweather.Util;
+
+import android.util.Log;
+
+import static com.about.switchweather.BuildConfig.*;
+
+/**
+ * Created by 跃峰 on 2016/9/7.
+ */
+public class LogUtils {
+    public static final boolean DEBUG = LOG_CALLS;
+
+    public static void i(String tag, String msg) {
+        if (DEBUG) {
+            Log.i(tag, msg);
+        }
+    }
+
+    public static void i(String tag, String msg, Throwable tr) {
+        if (DEBUG) {
+            Log.i(tag, msg, tr);
+        }
+    }
+
+    public static void e(String tag, String msg) {
+        if (DEBUG) {
+            Log.e(tag, msg);
+        }
+    }
+
+    public static void e(String tag, String msg, Throwable tr) {
+        if (DEBUG) {
+            Log.e(tag, msg, tr);
+        }
+    }
+}
