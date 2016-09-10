c9f1d2891bbef0dd4cfd84cb6994558bada973db
Vicent <mcxinyu@foxmail.com>
2016-09-10 23:08:09 +0800

    添加关于界面
    TODO
    12、后台凌晨更新，以获取全天天气信息；（100%）
    16、优化沉浸式状态栏；（80%）天气界面显示图片待完善
    17、设置界面及功能；（60%）功能待完善

--- app/src/main/java/com/about/switchweather/UI/SettingUI/SettingFragment.java
+++ app/src/main/java/com/about/switchweather/UI/SettingUI/SettingFragment.java
@@ -1,8 +1,11 @@
 package com.about.switchweather.UI.SettingUI;
 
 import android.os.Bundle;
+import android.preference.Preference;
 import android.preference.PreferenceFragment;
 import com.about.switchweather.R;
+import com.mikepenz.aboutlibraries.Libs;
+import com.mikepenz.aboutlibraries.LibsBuilder;
 
 /**
  * Created by 跃峰 on 2016/9/10.
@@ -23,5 +26,13 @@ public class SettingFragment extends PreferenceFragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.settings_general);
+
+        findPreference("setting_about").setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+            @Override
+            public boolean onPreferenceClick(Preference preference) {
+                new  LibsBuilder().withActivityStyle(Libs.ActivityStyle.LIGHT_DARK_TOOLBAR).start(getActivity());
+                return false;
+            }
+        });
     }
 }
